8548e45bfe4124a3f58d9947ad007e883a668011
Your Name <you@example.com>
2020-12-09 21:01:35 +0300

    construct function addItem

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ document.addEventListener('DOMContentLoaded',function () {
     const todos = [];
     const $todoForm = $('#todo-form');
 
-    $todoForm.on('submit', function(event) {
-        event.preventDefault();
+    function addItem(todoInputName){
         const formData = new FormData(this);
         const todoItem = {
             Status: false,
             Id: Math.random(),
-            Text: formData.get('todo-text'),
+            Text: formData.get(todoInputName),
         }
         todos.push(todoItem);
-        document.getElementById('myform').reset();
+        this.reset();
+    }
+
+    $todoForm.on('submit', function(event) {
+        event.preventDefault();
+        addItem.call(this,'todo-text');
     });
 })
